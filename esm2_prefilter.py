f8c8badc82b1759afec0c791473135cbf70bbc6d
rickyree <79752771+rickyree@users.noreply.github.com>
2026-04-01 23:12:20 +0100

    Add comment for future threshold automation work
    Added a comment regarding future work on threshold automation.

--- esm2_prefilter.py
+++ esm2_prefilter.py
@@ -571,6 +571,8 @@ def main():
     if out_dir:  # Only create directory if path contains one
         os.makedirs(out_dir, exist_ok=True)
     results_df = results_df.drop('contact_score', axis=1)
+
+    # future work: threshold will need to be automated e.g. with clustering methods per toxin
     results_df['flagged'] = results_df['chem_sim'] > 0.775
     results_df.to_csv(out_path, sep='\t', index=False)
 
